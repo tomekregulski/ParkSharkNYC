054fd6b7aa88e9cbe1d670a9e40540c9b8fdf611
Tomek <tomek.regulski@gmail.com>
2021-03-14 14:06:41 -0400

    Create parkResults array on script.js and set it as output from distance()

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var gpsBtn = document.getElementById('gps');
 var zipSearch = document.getElementById('go');
 var userLat;
 var userLon;
+var parkResults = [];
 
 // set click event listeners for buttons
 gpsBtn.addEventListener('click', getLocation);
@@ -50,19 +51,30 @@ function distance(lat1, lon1, lat2, lon2, park, parkIndex) {
     var a = 0.5 - c((lat2 - lat1) * p)/2 + 
             c(lat1 * p) * c(lat2 * p) * 
             (1 - c((lon2 - lon1) * p))/2;    
-    if ((12742 * Math.asin(Math.sqrt(a))) < 5 ) {
+    if ((12742 * Math.asin(Math.sqrt(a))) < 1 ) {
         // log index of each valid park, which can be passed to Map Marker and Search Results loop functions
+        var distance = (12742 * Math.asin(Math.sqrt(a)));
+        // console.log(distance);
         var closeParkIndex = parkIndex;
-        console.log(closeParkIndex);
+        // console.log(closeParkIndex);
         // Alternatively, we can set up each detail here as a variable to get passed to Map Marker and Search Results loop functions, whichever ends up being easier
-        var closeParkName = park
-        console.log(closeParkName);
-        var closeParkAddress = parksArray[closeParkIndex].address;
-        console.log(closeParkAddress);
-        var closeParkMonument = parksArray[closeParkIndex].monument;
-        console.log(closeParkMonument);
-        var closeParkTrail = parksArray[closeParkIndex].trails;
-        console.log(closeParkTrail);
+        // var closeParkName = park
+        // console.log(closeParkName);
+        // var closeParkAddress = parksArray[closeParkIndex].address;
+        // console.log(closeParkAddress);
+        // var closeParkMonument = parksArray[closeParkIndex].monument;
+        // console.log(closeParkMonument);
+        // var closeParkTrail = parksArray[closeParkIndex].trails;
+        // console.log(closeParkTrail);
+        var closePark = {};
+        closePark['name'] = park;
+        closePark['address'] = parksArray[closeParkIndex].address;
+        closePark['monuement'] = parksArray[closeParkIndex].monument;
+        closePark['trails'] = parksArray[closeParkIndex].trails;
+        closePark['distance'] = distance;
+        closePark['parksIndex'] = parkIndex;
+        // console.log(closePark);
+        parkResults.push(closePark);
         } 
     }; // 2 * R; R = 6371 km
 
