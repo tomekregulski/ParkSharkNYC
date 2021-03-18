d2996f873ed865b23c49ce23f0fcbcc6d57c428f
Sean Weber <sean.weber95@gmail.com>
2021-03-18 09:13:00 -0400

    polished styling heavily around bulma framework

--- assets/js/save.js
+++ assets/js/save.js
@@ -1,44 +1,43 @@
-//if storage is empty changes null value to empty string
 while (savedMem === null) {
-    var savedMem = [];
-}
+    savedMem = [];
+};
+
+var parkName = document.querySelector("#parkInput")
+var dateVisited = document.querySelector("#date-visited")
+var notes = document.querySelector("#textarea")
 
 //save function that activates upon save button click
-function handleSave(event) {
+function handleSave (event) {
     event.preventDefault();
-
     //returns user to form if not park name and visit date are not filled out
-    var parkName = document.querySelector("#park-name")
-    var dateVisited = document.querySelector("#date-visited")
-    var notes = document.querySelector("#saving-notes")
-
-    while (parkName.value === "" || dateVisited.value === ""){
+    
+    if (parkName.value === "" || dateVisited.value === ""){
         alert("Please enter a park name and visit date.")
         return;
-    }
+    } else {
+        while (notes.value ===""){
+            notes.value = "No notes.";
+        };
+    
+        //FormData API submission
+        const data = new FormData(event.target);
+    
+        const value = Object.fromEntries(data.entries());
 
-    while (notes.value ===""){
-        notes.value = "No notes.";
-    }
+        //logs form data to console
+        console.log(value);
+
+        //appends the memories object pulled from storage with new form data
+        savedMem.push(value);
+
+        //sends appended memories object back to storage as JSON
+        localStorage.setItem("memories", JSON.stringify(savedMem));
+
+        //reloads page on submission to update table
+        location.reload();
+    };
+  };
   
-    //FormData API submission
-    const data = new FormData(event.target);
-  
-    const value = Object.fromEntries(data.entries());
-
-    //logs form data to console
-    console.log(value);
-
-    //appends the memories object pulled from storage with new form data
-    savedMem.push(value);
-
-    //sends appended memories object back to storage as JSON
-    localStorage.setItem("memories", JSON.stringify(savedMem));
-
-    //reloads page on submission to update table
-    location.reload();
-  }
-  
-  //signals save function to run on save button click
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSave);
+//signals save function to run on save button click
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSave);
--- assets/js/table.js
+++ assets/js/table.js
@@ -4,6 +4,7 @@ var memJSON = localStorage.getItem("memories");
 var savedMem = [];
 
 console.log(memJSON);
+console.log(savedMem)
 
 //sorts objects in array by property
 function sortByProperty(prop) {
@@ -33,22 +34,25 @@ slider.addEventListener('change', function() {
         saveCheck();
         console.log(isChecked);
     } else {
-        savedMem.sort(sortByProperty("Park"));
+        savedMem.sort(sortByProperty("Name"));
         console.log(savedMem);
-        isChecked = true;
         loadMemories ();
+        isChecked = true;
         saveCheck();
         console.log(isChecked);
     }
-  });
+});
 
-  function saveCheck (){
+function saveCheck () {
     localStorage.setItem("slider", isChecked);
-}
+};
 
 window.onload = function() {
     slider = JSON.parse(localStorage.getItem("slider"));
-    savedMem = JSON.parse(localStorage.getItem("memories"))
+    savedMem = JSON.parse(localStorage.getItem("memories"));
+    while (savedMem === null) {
+        savedMem = [];
+    };
     if (isChecked === true) {
         savedMem.sort(sortByProperty("Park"));
         loadMemories();
@@ -58,7 +62,7 @@ window.onload = function() {
         loadMemories();
         return;
     }
-}
+};
 
 rawMem = [];
 
@@ -96,12 +100,14 @@ function loadMemories () {
 
     //creates table
     var table = document.createElement("table");
+    table.className = "table";
 
     //creates header row using extracted data
     var tr = table.insertRow(-1);
 
     for (var i = 0; i < col.length; i++) {
         var th = document.createElement("th");
+        th.className = "has-text-primary-light has-background-primary-dark"
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
@@ -113,6 +119,7 @@ function loadMemories () {
 
         for (var j = 0; j < col.length; j++) {
             var tabCell = tr.insertCell(-1);
+            tabCell.className = "has-text-primary-dark is-size-7"
             tabCell.innerHTML = sortMem[i][col[j]];
         }
     }
@@ -122,55 +129,43 @@ function loadMemories () {
     divShowData.innerHTML = "";
     divShowData.appendChild(table);
 
-
 };
 
+var x = document.getElementById("memPanel");
+var y = document.getElementById("myForm");
+var z = document.getElementById("clearSort");
+var w = document.getElementById("saveBtn");
+var s = document.getElementById("memBtn");
+
 //shows and hides the memories table on button click
-function showHide() {
-    
+function show() {
     if (savedMem == ""){
         alert("You have no saved memories.");
         return false;
     } else {
-        let x = document.getElementById("showData");
-        let y = document.getElementById("container");
-        let z = document.getElementById("slider round");
-        let w = document.getElementById("clear");
-        let u = document.querySelector("h4");
-
-        if (x.style.display === "none") {
-            x.style.display = "block";
-            y.style.display = "none";
-            z.style.display = "block";
-            w.style.display = "block";
-            u.style.display = "block";
-            var showButton = document.getElementById("show");
-            showButton.innerHTML = "Hide Memories";
-        } else {
-            x.style.display = "none";
-            y.style.display = "block";
-            z.style.display = "none";
-            w.style.display = "none";
-            u.style.display = "none";
-            var showButton = document.getElementById("show");
-            showButton.innerHTML = "Show Memories";
-        }
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
+        w.innerHTML = "back";
+        s.style.display = "none";
     }
   };
 
-  //clears memories
-  function clearMem () {
-    if (savedMem == ""){
-        alert("You have no saved memories.");
-        return false;
+function hide () {
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+    w.innerHTML = "";
+    s.style.display = "block";
+};
+
+//clears memories
+function clearMem () {
+    var warning = confirm("WARNING! You are about to erase all of your saved data. This cannot be undone. Do you want to proceed?");
+    if (warning === true){
+        localStorage.clear("memories");
+        location.reload();
     } else {
-        var warning = confirm("WARNING! You are about to erase all of your saved data. This cannot be undone. Do you want to proceed?");
-        if (warning === true){
-            localStorage.clear("memories");
-            location.reload();
-        }
-        else {
         return false;
-        }
     }
-  };
+};
