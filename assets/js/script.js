0b9170cba2b65aaeb28a40dfa3a45608d9ae2afc
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-06-07 11:24:36 -0400

    Installed Leaflet.js as map. Created parkCoords as hard reference for park lat/lon. Added informational tooltips for map results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,11 @@ var zipSearch = document.getElementById('go');
 var zipInput = document.getElementById('zip');
 var userLat;
 var userLon;
-var parkResults = [];
+var parkResults;
 var mapImage = document.getElementById('mapImg');
+var popupArray = [];
+var map;
+var markerLayer;
 
 // set click event listeners for buttons
 gpsBtn.addEventListener('click', getLocation);
@@ -25,10 +28,12 @@ function zipCoord(event)  {
         }
     }
     checkParkCoord();
+    
 };
 
 // capture user's current location and sets user lat/lon
 function getLocation() {
+    
     window.navigator.geolocation.getCurrentPosition((location) => {
           userLat = location.coords.latitude;
           userLon = location.coords.longitude;
@@ -38,50 +43,55 @@ function getLocation() {
   
 // pull coordinates of each park, to be checked for distance from user lat/lon
 function checkParkCoord() {
+    // clear parkResults array and remove any results cards from previous search
+    parkResults = [];
+    var resultsNode = document.getElementById("results");
+    resultsNode.innerHTML = '';
+    // create resultsArray for current search
     for (var i = 0; i < parksArray.length; i++) {
-        var parkLat = parksArray[i].coord[1];
-        var parkLon = parksArray[i].coord[0];
+        var parkLat = parseFloat(parksArray[i].lat);
+        var parkLon = parseFloat(parksArray[i].lon);
         var park = parksArray[i].name;
-        // capture index of each park into a variable
-        var parkIndex = i; 
-        distance(userLat, userLon, parkLat, parkLon, park, parkIndex);
+        var index = [i];
+        distance(userLat, userLon, parkLat, parkLon, park, index);
     }
-    // showMap() is intentionally commented out below for the purpose of Github deployment. 
-    //Please reference the Installation and Use section of the README for guidance on setting up functionality locally
-    // showMap();
     createResults();
+    showMap();
 };
 
 // Calculate the distance of each park from the search coordinates. Any park that is within one (1) mile will be pushed to the parkResults array and rendered as a search result
-function distance(lat1, lon1, lat2, lon2, park, parkIndex) {
+function distance(lat1, lon1, lat2, lon2, park, index) {
     var p = 0.017453292519943295; 
     var c = Math.cos;
     var a = 0.5 - c((lat2 - lat1) * p)/2 + 
             c(lat1 * p) * c(lat2 * p) * 
-            (1 - c((lon2 - lon1) * p))/2;    
+            (1 - c((lon2 - lon1) * p))/2;
     if ((12742 * Math.asin(Math.sqrt(a))) < 1 ) {
-        // var distance = (12742 * Math.asin(Math.sqrt(a)));
-        var closeParkIndex = parkIndex;
+        console.log('hello distance'); 
+        console.log(parksArray[index].location);
         var closePark = {};
         closePark['name'] = park;
-        closePark['address'] = parksArray[closeParkIndex].address;
-        closePark['monument'] = parksArray[closeParkIndex].monument;
-        closePark['trails'] = parksArray[closeParkIndex].trails;
+        closePark['location'] = parksArray[index].location;
+        closePark['monument'] = parksArray[index].monument;
+        closePark['trails'] = parksArray[index].trails;
+        closePark['lat'] = parksArray[index].lat;
+        closePark['lon'] = parksArray[index].lon;
         parkResults.push(closePark);
     } 
 }; 
 
 function createResults() {
     //render park search results in results div
-    for (var i = 0; i < parkResults.length; i++) {//parksArray.length
+    for (var i = 0; i < parkResults.length; i++) {
         console.log("rendering div now")
         //create result card elements
         var nameList = document.createElement('p');
-        var address = document.createElement('p');
+        var location = document.createElement('p');
         var monumentList = document.createElement('p');
         var trailList = document.createElement('p');
         nameList.innerText = parkResults[i].name;
-        address.innerText = parkResults[i].address;
+        location.innerText = parkResults[i].location;
+        console.log(parkResults[i].location);
         monumentList.innerText = 'Monuments: ' + parkResults[i].monument
         trailList.innerText = 'Trails: ' + parkResults[i].trails
 
@@ -89,13 +99,13 @@ function createResults() {
         var parksdiv = document.createElement("div");
         
         parksdiv.appendChild(nameList);
-        parksdiv.appendChild(address);
+        parksdiv.appendChild(location);
         parksdiv.appendChild(monumentList);
         parksdiv.appendChild(trailList);
         
         parksdiv.setAttribute("class", "panel has-background-primary-light resultCard");
         nameList.setAttribute('class', 'panel-heading has-background-primary-dark has-text-primary-light');
-        address.setAttribute('class', 'subtitle-is-7 has-text-primary-dark has-text-left')
+        location.setAttribute('class', 'subtitle-is-7 has-text-primary-dark has-text-left')
         monumentList.setAttribute('class', 'control has-text-primary-dark has-text-left');
         trailList.setAttribute('class', 'control has-text-primary-dark has-text-left');
 
@@ -105,3 +115,51 @@ function createResults() {
         resultEl.setAttribute('class', 'box p-4');
     }
 };
+
+// Reset map layer if present from a previous search
+function checkMap() {
+    var container = L.DomUtil.get('map');
+    var mapDiv = document.getElementById('map');
+        if(container != null){
+            container._leaflet_id = null;
+            mapDiv.setAttribute('class', 'map-div.show');
+        }
+}
+
+// Show the map, centered on the user coordinates
+function showMap() {
+    checkMap();
+    var map = L.map('map').setView([userLat, userLon], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    createMarkers(map);
+}
+
+// Create and place the markers for each park in parkResults
+function createMarkers(map) {
+    popupArray = [];
+    console.log("creating popups");
+    for (var i = 0; i < parkResults.length; i++) {
+        var lon = parkResults[i].lon;
+        var lat = parkResults[i].lat;
+        console.log(lat);
+        console.log(lon);
+        var name = parkResults[i].name;
+        var location = parkResults[i].location;
+        var monument = parkResults[i].monument;
+        var trail = parkResults[i].trails;
+        console.log(name, location, monument, trail);
+        var popupContent = `${name}`;
+        console.log(popupContent);
+        
+        var markerLocation = new L.LatLng(lat, lon);
+        var marker = new L.Marker(markerLocation);
+
+        marker.bindTooltip(`${name}<br>${location}<br>Monuments: ${monument}<br>Trails: ${trail}`)
+        popupArray.push(marker)
+        
+    }
+    var markerLayer = L.layerGroup(popupArray);
+    map.addLayer(markerLayer);
+}
